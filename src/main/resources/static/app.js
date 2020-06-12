dd7e8994f4cace3ac21a999872c452d5c1ca0f7b
Yulong Shi <yu.long.shi@oracle.com>
2020-06-12 16:48:02 +0800

    make the chessboard fixed during moving

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -52,6 +52,7 @@ function chessCallback(chessResponse) {
         console.log("received：" + JSON.stringify(retObj.payload.activePlayer));
         loadChessboard(retObj.payload.chessboard);
         updatePlayerInfo(retObj.payload.activePlayer);
+        checkGameState(retObj.payload.state);
     } else {
         console.log("received: " + retObj.message);
     }
@@ -69,29 +70,54 @@ function showGreeting(message) {
 }
 
 function loadChessboard(chessboard) {
-    let table = $('#chessboard');
-    table.find("tbody tr").remove();
+    //let table = $('#chessboard');
+    //table.find("tbody tr").remove();
     for (let i = 0; i < chessboard.grid.length; i++) {
-        let fields = "";
+        // let fields = "";
         for (let j = 0; j < chessboard.grid[i].length; j++) {
+
             let piece = chessboard.grid[i][j];
+
             let pos = "pos_" + i + "_" + j;
+            let td = $("#" + pos);
+
             if (piece) {
-                let cls = piece.color == 'Red' ? 'text-danger' : 'text-nothing';
-                let name = piece.hidden ? '&nbsp;&nbsp;' : piece.name
-                fields += "<td id='" + pos + "' class='bg-info font-weight-bold " + cls + "'>" + name + "</td>"
+                td.addClass("bg-info");
+
+                if (piece.hidden) {
+                    td.html("<div class='piece text-muted'>秘</div>");
+                } else {
+                    let color = piece && piece.color == 'Red' ? 'red' : 'black';
+                    td.html("<div class='piece font-bold-" + color + "'>" + piece.name + "</div>");
+                }
             } else {
-                fields += "<td id='" + pos + "'></td>"
+                td.removeClass("bg-info");
+                td.html("<div class='piece blank'>空</div>")
             }
+
+            // if (piece) {
+            //     let cls = piece.color == 'Red' ? 'text-danger' : 'text-nothing';
+            //     let name = piece.hidden ? '&nbsp;' : piece.name
+            //     fields += "<td data-pos=" + pos + " id='" + pos + "' class='bg-info font-weight-bold " + cls + "'>" + name + "</td>"
+            // } else {
+            //     fields += "<td data-pos=" + pos + " id='" + pos + "'>&nbsp;</td>"
+            // }
         }
-        table.append("<tr>" + fields + "</tr>");
+        //table.append("<tr>" + fields + "</tr>");
+    }
+}
+
+function checkGameState(state) {
+    if(state == "END") {
+        $("#activePlayer").html("<div>Game Over</div>");
     }
 }
 
 function updatePlayerInfo(player) {
     if (player) {
         let color = player.color ? (player.color == 'Red' ? "红方" : "黑方") : "颜色未定";
-        $("#activePlayer").html("等待<b>" + player.name + "（" + color + "）</b>走子...")
+        let cls = player.color && player.color == 'Red' ? 'font-bold-red' : 'font-bold-black';
+        $("#activePlayer").html("等待 <span class='" + cls + "'>" + player.name + "（" + color + "</span>）走子...")
     } else {
         $("#activePlayer").empty();
     }
@@ -116,8 +142,17 @@ $(function () {
     $("#send3").click(function () {
         playGame();
     });
-    $("td").mouseover(function () {
-        console.log("clicked: " + this.id);
+    $("#chessboard tr td").mouseover(function () {
+
+    });
+    $("#chessboard tr td").click(function () {
+        let row = $(this).attr('data-row');
+        let col = $(this).attr('data-col');
+        console.log('data-pos = ' + row + ',' + col);
+        stompClient.send("/app/game/action/" + gameId, {}, JSON.stringify({
+            'player': {'name': $("#player3").val()},
+            'position': {'row': row, 'col': col}
+        }));
     });
 });
 
